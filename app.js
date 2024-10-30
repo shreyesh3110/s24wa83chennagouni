08f13c09f7e585ee60d2f937639f35d501199df6
shreyesh3110 <s576283@nwmissouri.edu>
2024-10-30 13:08:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use('/mydata', mydataRouter);
 app.use(express.urlencoded({ extended: false }));
+app.use('/mydata', mydataRouter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 

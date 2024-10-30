0d066733aefa189b52bcee722fcf9080c90767b7
shreyesh3110 <s576283@nwmissouri.edu>
2024-10-30 15:59:28 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shreyesh Chennagouni' });
+  res.render('mydata', { title: 'Shreyesh Chennagouni' });
 });
 
 module.exports = router;
